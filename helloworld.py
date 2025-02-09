72130fc1924a9527f0d66acb18f1a2c2438bcef3
unknown <hugoandrevanzyl@gmail.com>
2025-02-09 17:00:52 +0000

    Week 01 comments and resources update

--- helloworld.py
+++ helloworld.py
@@ -1,1 +1,11 @@
-print  ("Hello World")
+# helloworld.py
+# This program just prints a predefined message - Hellow World
+# author: Hugo van Zyl
+
+print  ("Hello World") #Python function used to print something
+
+'''
+Resources:
+The following resources were used in completing this task:
+1. Week 1 Lecture videos
+'''
